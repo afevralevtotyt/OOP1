4d21f79e698a0c336f14e0b6400b755c17f3842b
afevralevtotyt <afevraleff@ya.ru>
2022-10-19 13:49:17 +0200

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,20 @@
+public class Car {
+    String  brand;
+    String model;
+    double engineVolume;
+    String color;
+    int productionYear;
+    String productionCountry;
+
+    public Car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.productionYear = productionYear;
+        this.productionCountry = productionCountry;
+    }
+
+
+
+}
